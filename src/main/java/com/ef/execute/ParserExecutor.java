77a45785cec1608f01d04288954e66a9da47a697
emerson <emerson@unovation.com.br>
2018-10-01 22:07:05 -0300

    add: create simple test for all input parameters

--- src/main/java/com/ef/execute/ParserExecutor.java
+++ src/main/java/com/ef/execute/ParserExecutor.java
@@ -3,6 +3,7 @@ package com.ef.execute;
 import com.ef.extract.ColumnExtractor;
 import com.ef.extract.LogColumn;
 import com.ef.input.ConsoleInput;
+import com.ef.input.Duration;
 import com.ef.input.InputType;
 import com.ef.input.InputValidator;
 import com.ef.input.StartDate;
@@ -10,16 +11,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
+import static com.ef.input.InputType.DURATION;
 import static com.ef.input.InputType.START_DATE;
+import static com.ef.input.InputType.THRESHOLD;
 
 public class ParserExecutor {
 
     public static final String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final int ONE = 1;
+    public static final long ZERO = 0L;
     private InputValidator inputValidator;
     private ParserFile file;
     private ConsoleInput consoleInput;
+
+    private ConcurrentHashMap<String, Long> cache = new ConcurrentHashMap<>();
 
     public ParserExecutor(InputValidator inputValidator,
                           ParserFile file,
@@ -33,23 +41,36 @@ public class ParserExecutor {
         inputValidator.validate(args);
         Map<InputType, String> consoleInputs = consoleInput.extract(args);
         String path = consoleInputs.get(InputType.ACCESS_LOG);
-        String startDate = consoleInputs.get(START_DATE);
+        String threshold = consoleInputs.get(THRESHOLD);
         return file.lines(path)
-                .filter(line -> dateInLineIsAfter(line, startDate))
+                .filter(line -> dateInLineIsAfter(line, consoleInputs))
+                .filter(line -> countIp(line, threshold))
                 .map(this::getIP).collect(Collectors.toList());
     }
 
-    private boolean dateInLineIsAfter(String line, String startDateStr){
+    private boolean countIp(String line, String threshold) {
+        String ip = getIP(line);
+        cache.put(ip,cache.getOrDefault(ip, ZERO) + ONE);
+        return cache.get(ip) >= Integer.valueOf(threshold);
+    }
+
+    private boolean dateInLineIsAfter(String line, Map<InputType, String> consoleInputs){
         String date = new ColumnExtractor().columnOnLine(LogColumn.DATE, line);
-        LocalDateTime startDate = getDate(startDateStr, StartDate.DATE_FORMAT);
+        String duration = consoleInputs.get(DURATION);
+        LocalDateTime startDate = getDate(consoleInputs.get(START_DATE), StartDate.DATE_FORMAT);
+        LocalDateTime tempDate = LocalDateTime.now();
+        if(Duration.HOURLY.equals(duration)){
+            tempDate = startDate.plusHours(1);
+        }
+        if(Duration.DAILY.equals(duration)){
+            tempDate = startDate.plusDays(1);
+        }
         LocalDateTime lineDate = getDate(date, YYYY_MM_DD_HH_MM_SS_SSS);
-        return lineDate.isAfter(startDate);
+        return lineDate.isBefore(tempDate) && lineDate.isAfter(startDate);
     }
 
     private String getIP(String line){
-        String ip = new ColumnExtractor().columnOnLine(LogColumn.IP, line);
-        System.out.println(ip);
-        return ip;
+        return new ColumnExtractor().columnOnLine(LogColumn.IP, line);
     }
 
     private LocalDateTime getDate(String date, String pattern) {
@@ -57,3 +78,4 @@ public class ParserExecutor {
         return LocalDateTime.parse(date, lineDateFormatter);
     }
 }
+
--- src/test/groovy/com/ef/execute/ParserExecutorTest.groovy
+++ src/test/groovy/com/ef/execute/ParserExecutorTest.groovy
@@ -17,21 +17,103 @@ class ParserExecutorTest  extends Specification {
         parserExecutor = new ParserExecutor(validatorMock, fileMock, createInput())
     }
 
-    def 'given a valid line when extract date should be returned'(){
+    def 'given a valid line when startDate match should return IP result'(){
         given:
         String expectedIp = '192.168.234.82'
         String path = '/path/to/file'
         Stream fileContent = Stream.of(
-                """2017-01-01 00:00:11.763|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version) CFNetwork/808.2.16 Darwin/15.6.0""".toString(),
-                """2017-01-01 00:00:21.164|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version) CFNetwork/808.2.16 Darwin/15.6.0""".toString()
+                """2017-01-01 00:00:11.763|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:00:21.164|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString()
         )
         fileMock.lines(path) >> fileContent
-        String[] args = ["--accesslog=${path}",'--startDate=2017-01-01.00:00:00','--duration=hourly','--threshold=1']
+        String[] args = ["--accesslog=${path}",'--startDate=2017-01-01.00:00:00','--duration=hourly','--threshold=2']
 
         when:
         def result = parserExecutor.execute(args)
 
         then:
-        result.count { it == expectedIp } == 2
+        result.count { it == expectedIp } == 1
+    }
+
+    def 'given a valid line should consider only IPs with threshold match'(){
+        given:
+        String expectedIp = '192.168.234.82'
+        String path = '/path/to/file'
+        Stream fileContent = Stream.of(
+                """2017-01-01 00:00:11.763|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:15:21.164|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:30:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString(),
+                """2017-01-01 00:35:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString()
+        )
+        fileMock.lines(path) >> fileContent
+        String[] args = ["--accesslog=${path}",'--startDate=2017-01-01.00:00:00','--duration=hourly','--threshold=4']
+
+        when:
+        def result = parserExecutor.execute(args)
+
+        then:
+        result.count { it == expectedIp } == 1
+    }
+
+
+    def 'lines with time after hourly duration should not be considered'(){
+        given:
+        String expectedIp = '192.168.234.82'
+        String path = '/path/to/file'
+        Stream fileContent = Stream.of(
+                """2017-01-01 00:00:11.763|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:15:21.164|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:30:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString(),
+                """2017-01-01 01:30:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString()
+        )
+        fileMock.lines(path) >> fileContent
+        String[] args = ["--accesslog=${path}",'--startDate=2017-01-01.00:00:00','--duration=hourly','--threshold=4']
+
+        when:
+        def result = parserExecutor.execute(args)
+
+        then:
+        result.count { it == expectedIp } == 0
+    }
+
+    def 'lines with time after daily duration should not be considered'(){
+        given:
+        String expectedIp = '192.168.234.82'
+        String path = '/path/to/file'
+        Stream fileContent = Stream.of(
+                """2017-01-01 00:00:11.763|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:15:21.164|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:30:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString(),
+                """2017-01-02 00:30:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString()
+        )
+        fileMock.lines(path) >> fileContent
+        String[] args = ["--accesslog=${path}",'--startDate=2017-01-01.00:00:00','--duration=daily','--threshold=4']
+
+        when:
+        def result = parserExecutor.execute(args)
+
+        then:
+        result.count { it == expectedIp } == 0
+    }
+
+
+    def 'given a valid lines without valid threshold match should not return IPs'(){
+        given:
+        String expectedIp = '192.168.234.82'
+        String path = '/path/to/file'
+        Stream fileContent = Stream.of(
+                """2017-01-01 00:00:11.763|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:15:21.164|${expectedIp}|"GET / HTTP/1.1"|200|"swcd (unknown version)""".toString(),
+                """2017-01-01 00:30:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString(),
+                """2017-01-01 00:38:59.410|${expectedIp}|"GET / HTTP/1.1"|200|"Mozilla/5.0""".toString()
+        )
+        fileMock.lines(path) >> fileContent
+        String[] args = ["--accesslog=${path}",'--startDate=2017-01-01.00:00:00','--duration=hourly','--threshold=10']
+
+        when:
+        def result = parserExecutor.execute(args)
+
+        then:
+        result.isEmpty()
     }
 }
